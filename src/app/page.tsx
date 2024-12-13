3021b7d446565722bca9a702c2b641af5417aeb9
rasoolmir <rasool.mir634@gmail.com>
2024-12-13 05:38:32 -0800

    [Add] dynamic route example

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,26 @@
-export default function Home() {
-  return <h1>!سلام رفیق</h1>; 
+type Post = {
+  id: number;
+  title: string;
+};
+
+export default async function Home() {
+  const res :Response = await fetch("https://jsonplaceholder.org/posts", {
+    cache: "no-store",
+  });
+
+  const posts: Post[] = await res.json();
+
+  return (
+    <main>
+      <h1>!سلام، رفیق</h1>
+      <p>
+        لورم
+      </p>
+      <ul>
+       {posts.map((post) => (
+          <li key={post.id}>{post.title}</li>
+        ))}
+      </ul>
+    </main>
+  );
 }
