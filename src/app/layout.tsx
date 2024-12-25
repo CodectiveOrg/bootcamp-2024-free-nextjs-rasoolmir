416d90ce4b1a895dbe94bfcb03d5696b7d8eafe1
rasoolmir <rasool.mir634@gmail.com>
2024-12-24 23:01:53 -0800

    [ADD] header and footer

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,11 @@
 import type { Metadata } from "next";
 import { Vazirmatn } from "next/font/google";
+
+import HeaderComponent from "@/components/header/header.component";
+import FooterComponent from "@/components/footer/footer.component";
+
 import "./globals.css";
+
 
 const vazirmatn = Vazirmatn({
   subsets: ["latin", "arabic"],
@@ -19,7 +24,14 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="fa" dir="rtl" className={vazirmatn.className}>
-      <body>{children}</body>
+      <body>
+        <HeaderComponent />
+        <main>{children}</main>
+        <p className="tagline">
+          نوبت دهی پزشکی، سامانه نوبت دهی اینترنتی بیمارستان و پزشکان
+        </p>
+        <FooterComponent />
+      </body>
     </html>
   );
 }
