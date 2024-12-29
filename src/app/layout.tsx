2f5322c2d50de72011e3784ceccd7d7eb6ab0410
Rasool Mirmojarabiyan <138984913+rasoolmir@users.noreply.github.com>
2024-12-29 00:24:58 -0800

    Lesson 2 (#3)
    * [ADD] prettierrc
    
    * [PRETTIER]
    
    * [CI/CD] lint.yml
    
    * [ADD] postcss nesting
    
    * [ADD] css reset
    
    * [REMOVE] unnecessary code and file
    
    * [UPDATE] favicon
    
    * [ADD] header and footer
    
    * [UPDATE] home page
    
    * [CHANGE] logo icon
    
    * [MERGE] conflict
    
    * [UPDATE] CONF
    
    * Update lint.yml
    
    * Update package.json
    
    * Update package-lock.json
    
    * Update lint.yml

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,11 @@
+import { ReactElement } from "react";
+
 import type { Metadata } from "next";
 import { Vazirmatn } from "next/font/google";
+
+import HeaderComponent from "@/components/header/header.component";
+import FooterComponent from "@/components/footer/footer.component";
+
 import "./globals.css";
 
 const vazirmatn = Vazirmatn({
@@ -8,18 +14,25 @@ const vazirmatn = Vazirmatn({
 });
 
 export const metadata: Metadata = {
-  title: "دکتر یاب",
-  description: "بلتفرم جامع جستجوی دگتر و رزرو نوبت آنلاین",
+  title: "دکتر من",
+  description: "پلتفرم جامع جستجوی دکتر و رزرو نوبت آنلاین",
 };
 
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
-}>) {
+}>): ReactElement {
   return (
     <html lang="fa" dir="rtl" className={vazirmatn.className}>
-    <body>{children}</body>
-  </html>
+      <body>
+        <HeaderComponent />
+        <main>{children}</main>
+        <p className="tagline">
+          نوبت دهی پزشکی، سامانه نوبت دهی اینترنتی بیمارستان و پزشکان
+        </p>
+        <FooterComponent />
+      </body>
+    </html>
   );
 }
