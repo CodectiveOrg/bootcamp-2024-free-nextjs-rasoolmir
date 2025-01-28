4151db61549f4cfedc23df130929fbc7221cbb84
rasoolmir <rasool.mir634@gmail.com>
2025-01-28 00:14:22 -0800

    [FIX] wrong SearchParams type

--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -22,13 +22,13 @@ import styles from "./page.module.css";
 type SearchParams = { [key: string]: string | string[] | undefined };
 
 type Props = {
-  searchParams: Promise<SearchParams>;
+  searchParams: SearchParams;
 };
 
-export default async function Page({
+export default function Page({
   searchParams,
-}: Props): Promise<ReactElement> {
-  const defaultFilters = generateDefaultFilters(await searchParams);
+}: Props): ReactElement {
+  const defaultFilters = generateDefaultFilters(searchParams);
 
   return (
     <FiltersProvider defaultFilters={defaultFilters}>
