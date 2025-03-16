96ca2d9556588e94aa19a9584bb90b2d1c678ea8
rasoolmir <rasool.mir634@gmail.com>
2025-03-16 09:09:46 +0000

    [ADD] username and email validation

--- src/app/api/auth/sign-up/route.ts
+++ src/app/api/auth/sign-up/route.ts
@@ -16,6 +16,26 @@ export async function Post(request: Request): Promise<ApiResponseType<null>> {
       return NextResponse.json({ error: parseError }, { status: 400 });
     }
 
+    let foundUser = await prisma.user.findUnique({
+      where: { username: body.username },
+    });
+
+    if (foundUser) {
+      return NextResponse.json(
+        { error: "The username is already taken." },
+        { status: 400 },
+      );
+    }
+
+    foundUser = await prisma.user.findUnique({
+      where: { email: body.email },
+    });
+
+    if (foundUser) {
+      return NextResponse.json({ error: "The email is already in use." }, { status: 400 });
+    }
+
+
     await prisma.user.create({ data: body });
 
     return NextResponse.json({ data: null }, { status: 201 });
