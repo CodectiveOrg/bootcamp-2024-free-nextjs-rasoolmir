b74e109d6e1499c4fbb79a44362aa6dec1b023b4
rasoolmir <rasool.mir634@gmail.com>
2025-03-16 08:56:40 +0000

    [ADD] parseBody

--- src/app/api/auth/sign-up/route.ts
+++ src/app/api/auth/sign-up/route.ts
@@ -4,12 +4,17 @@ import prisma from "@/lib/prisma";
 
 import { ApiResponseType } from "@/types/api-respones.type";
 
-import { wrapWithTryCatch } from "@/utils/api.utils";
+import { parseBody, wrapWithTryCatch } from "@/utils/api.utils";
+import { SignUpDto } from "@/dto/auth.dto";
 
 
 export async function Post(request: Request): Promise<ApiResponseType<null>> {
   return wrapWithTryCatch(async () => {
-    const body = await request.json();
+    const [parseError, body] = await parseBody<SignUpDto>(request);
+
+    if (parseError !== null) {
+      return NextResponse.json({ error: parseError }, { status: 400 });
+    }
 
     await prisma.user.create({ data: body });
 
--- src/utils/api.utils.ts
+++ src/utils/api.utils.ts
@@ -1,17 +1,43 @@
-import { ApiResponseType } from "@/types/api-respones.type";
 import { NextResponse } from "next/server";
+import { ApiResponseType } from "@/types/api-respones.type";
+
+type ParseBodyResult<T> = [error: null, data: T] | [error: string, data: null];
+
+export async function parseBody<T>(
+  request: Request,
+): Promise<ParseBodyResult<T>> {
+  try {
+    const body = await request.json();
+    return [null, body];
+  } catch (error) {
+    if (error instanceof Error) {
+      if (error.name === "SyntaxError") {
+        return ["The body format is incorrect.", null];
+      }
+
+      return [error.message, null];
+    }
+
+    if (typeof error === "string") {
+      return [error, null];
+    }
+
+    return ["An unexpected error occurred.", null];
+  }
+}
 
 export async function wrapWithTryCatch<T>(
-  callback: () => Promise< ApiResponseType<T>>,
-): Promise<ApiResponseType <T>> {
+  callback: () => Promise<ApiResponseType<T>>,
+): Promise<ApiResponseType<T>> {
   try {
     return await callback();
   } catch (error) {
     if (error instanceof Error) {
       return NextResponse.json({ error: error.message }, { status: 400 });
     }
+
     return NextResponse.json(
-      { error: "Something went wrong" },
+      { error: "An unexpected error occurred." },
       { status: 500 },
     );
   }
