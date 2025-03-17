5ea337b317102cb948939c3358d8930cd45b3d9d
rasoolmir <rasool.mir634@gmail.com>
2025-03-17 19:33:38 +0000

    [ADD] SIGN IN API

--- src/app/auth/components/sign-in-form/sign-in-form.component.tsx
+++ src/app/auth/components/sign-in-form/sign-in-form.component.tsx
@@ -1,9 +1,10 @@
 "use client";
 
-import { FormEvent, ReactElement } from "react";
+import { FormEvent, ReactElement, useRef } from "react";
 
 import Image from "next/image";
 import Link from "next/link";
+import { useRouter } from "next/navigation";
 
 import signInImage from "@/assets/images/sign-in.webp";
 
@@ -11,16 +12,50 @@ import { ButtonComponent } from "@/components/button/button.component";
 import CardComponent from "@/components/card/card.component";
 import NormalInputComponent from "@/components/normal-input/normal-input.component";
 import PasswordInputComponent from "@/components/password-input/password-input.component";
+
+import { fetchWithToast } from "@/utils/fetch-utils";
+
 import MingcuteUser3Line from "@/icons/MingcuteUser3Line";
+
+import { SignInDto } from "@/dto/auth.dto";
 
 import styles from "@/app/auth/styles/auth-form.module.css";
 
 export default function SignInFormComponent(): ReactElement {
+
+  const router = useRouter();
+
+  const formRef = useRef<HTMLFormElement>(null);
+
   const formSubmitHandler = async (
     e: FormEvent<HTMLFormElement>,
   ): Promise<void> => {
     e.preventDefault();
+
+    const formData = new FormData(e.currentTarget);
+
+    const dto: SignInDto = {
+      username: formData.get("username") as string,
+      password: formData.get("password") as string,
+    };
+
+    const result = await fetchWithToast<null>(
+      "/api/auth/sign-in",
+      {
+        method: "POST",
+        body: JSON.stringify(dto),
+      },
+      "Welcome back!😍",
+    );
+
+    if (result.error) {
+      return;
+    }
+
+    formRef.current?.reset();
   };
+
+  
 
   return (
     <div className={styles["auth-form"]}>
@@ -31,7 +66,7 @@ export default function SignInFormComponent(): ReactElement {
           </div>
           <div className={styles.writings}>
             <h1>Sign In</h1>
-            <form onSubmit={formSubmitHandler}>
+            <form ref={formRef} onSubmit={formSubmitHandler}>
               <NormalInputComponent
                 label="User Name"
                 type="text"
